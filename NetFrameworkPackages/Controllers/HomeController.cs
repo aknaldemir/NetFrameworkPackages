950d26bc02e9c5d2206c3a73f00856a50c1aafcc
aknaldemir <aknaldemir@gmail.com>
2023-02-22 19:10:08 +0300

    Add project files.

---
+++ NetFrameworkPackages/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NetFrameworkPackages.Data;
+
+namespace NetFrameworkPackages.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MyContext context= new MyContext();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
